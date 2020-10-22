abe24682a05e08d2bb1053cebe05dc687c77cceb
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-22 00:34:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,10 @@ class TicTacToe
     input.to_i - 1
   end
 
+  def valid_move?(board, index)
+    index.between?(0, 8) && !position_taken?(board, index)
+  end
+
   def position_taken?(board, index)
     !(board[index].nil? || board[index] == ' ')
   end
