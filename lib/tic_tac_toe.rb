0abf7fec2045d9f4a83bc3d29e92b2715ffc4dae
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-22 00:42:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,18 +30,18 @@ class TicTacToe
     index.between?(0, 8) && !position_taken?(board, index)
   end
 
-  def position_taken?(board, index)
-    !(board[index].nil? || board[index] == ' ')
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == ' ')
   end
 
   def move(pos, char)
     @board[pos] = char
   end
 
-  def turn_count(board)
+  def turn_count
     num_turns = 0
 
-    board.each do |val|
+    @board.each do |val|
       if val == 'X' || val == 'O'
         num_turns += 1
       end
@@ -50,36 +50,36 @@ class TicTacToe
     num_turns
   end
 
-  def current_player(board)
-    if turn_count(board).even?
+  def current_player
+    if turn_count.even?
       return 'X'
     end
 
     'O'
   end
 
-  def turn(board)
+  def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
 
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
-      display_board(board)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
     else
-      turn(board)
+      turn
     end
   end
 
-  def won?(board)
+  def won?
     WIN_COMBINATIONS.each do |win_combination|
       win_index_1 = win_combination[0]
       win_index_2 = win_combination[1]
       win_index_3 = win_combination[2]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
 
       if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
         return win_combination
@@ -89,17 +89,17 @@ class TicTacToe
     false
   end
 
-  def full?(board)
-    board_full = board.all? do |index|
+  def full?
+    board_full = @board.all? do |index|
       index == 'O' || index == 'X'
     end
 
     board_full
   end
 
-  def draw?(board)
-    full = full?(board)
-    won = won?(board)
+  def draw?
+    full = full?
+    won = won?
 
     if full && !won
       return true
@@ -108,30 +108,30 @@ class TicTacToe
     end
   end
 
-  def over?(board)
-    won?(board) || full?(board) || draw?(board)
+  def over?
+    won? || full? || draw?
   end
 
-  def winner(board)
-    won = won?(board)
+  def winner
+    won = won?
 
     if won
-      return board[won[0]]
+      return @board[won[0]]
     end
 
     nil
   end
 
-  def play(board)
+  def play
     counter = 0
 
-    until over?(board)
-      turn(board)
+    until over?
+      turn
     end
 
-    if won?(board)
-      puts "Congratulations #{winner(board)}!"
-    elsif draw?(board)
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
       puts "Cat's Game!"
     end
   end
